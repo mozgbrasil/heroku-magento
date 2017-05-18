3b266bed99bb088c43d93cbf2ce5dabdfd8fbff9
mozgbrasil <mozgbrasil@gmail.com>
2017-05-18 19:15:30 -0300

    commit - 18/05/2017 19:14:25

--- app.sh
+++ app.sh
@@ -2,22 +2,22 @@
 
 #
 
-echo -e "--(-)--"
+echo -e "--(1)--"
 
 pwd
 
-echo -e "--(-)--"
+echo -e "--(2)--"
 
 ls
 
-echo -e "--(-)--"
+echo -e "--(3)--"
 
 echo $1
 
-echo -e "--(-)--"
+echo -e "--(4)--"
 
 curl --request POST "https://fleep.io/hook/OLuIRi0JRt2yv5OQisX6tg" --data $1
 
-echo -e "--(-)--"
+echo -e "--(5)--"
 
 #
