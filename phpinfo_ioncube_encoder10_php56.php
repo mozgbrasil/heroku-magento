5e5072821e756b49915f3dc503fdf4ab4baa7ab0
mozgbrasil <mozgbrasil@gmail.com>
2017-09-01 16:11:08 -0300

    test

---
+++ phpinfo_ioncube_encoder10_php56.php
@@ -0,0 +1,13 @@
+<?php //004fb
+if(!extension_loaded('ionCube Loader')){$__oc=strtolower(substr(php_uname(),0,3));$__ln='ioncube_loader_'.$__oc.'_'.substr(phpversion(),0,3).(($__oc=='win')?'.dll':'.so');if(function_exists('dl')){@dl($__ln);}if(function_exists('_il_exec')){return _il_exec();}$__ln='/ioncube/'.$__ln;$__oid=$__id=realpath(ini_get('extension_dir'));$__here=dirname(__FILE__);if(strlen($__id)>1&&$__id[1]==':'){$__id=str_replace('\\','/',substr($__id,2));$__here=str_replace('\\','/',substr($__here,2));}$__rd=str_repeat('/..',substr_count($__id,'/')).$__here.'/';$__i=strlen($__rd);while($__i--){if($__rd[$__i]=='/'){$__lp=substr($__rd,0,$__i).$__ln;if(file_exists($__oid.$__lp)){$__ln=$__lp;break;}}}if(function_exists('dl')){@dl($__ln);}}else{die('The file '.__FILE__." is corrupted.\n");}if(function_exists('_il_exec')){return _il_exec();}echo("Site error: the ".(php_sapi_name()=='cli'?'ionCube':'<a href="http://www.ioncube.com">ionCube</a>')." PHP Loader needs to be installed. This is a widely used PHP extension for running ionCube protected PHP code, website security and malware blocking.\n\nPlease visit ".(php_sapi_name()=='cli'?'get-loader.ioncube.com':'<a href="http://get-loader.ioncube.com">get-loader.ioncube.com</a>')." for install assistance.\n\n");exit(199);
+?>
+HR+cPwoG9bE/PfjaHYkpSBX4blqbshf/Clea+Vqv/Twyq9JG7DcvSwiCpDK8wrsFRquJlk4n59+p
+GgDrpft+k5mctGPYVuOny/OfBAmWmBq+M1Cw9OkcpbRCvnIArouRlL3Aznlmvgl467dSKqbPBBLl
+ScCQo+a0u1cCNpJHz0AwhvOCtNtCSeVRdNJ9OHHuT9rnHe+X0L/oILEHBn7lRwZALj63ZrA9yDJy
+NxuSfQudaFA1PKUxtzLX+GrimZXb8M2z/sTyFrBXj94HMQe2qau1od0gprCl2MtAD3kKVYLM58je
+k8g6sXDgBJgH4rQUDJzqKu3SMt7VJp5RmpzN3Hv21e5x9MRMG9pUUGWwssPwJ0dT7GS07nAjxk3Z
+lK2dXDHHkQXb4Px/Iz0F347tJa8NTbiSABGKftAskSAgxENYQsTxQM4MslnTTwh+uEzKxgLk2vFq
+PfGb836fkMidDMdCJoEvdkagge9g27MdqZwXxkFa9K/ymTb1Y+OnJj+9ohBUB2Qg5i55e13OA/w7
+eIaHdgOjJoUrbWaH3x3t/hqaqQhEBLOCzghWJ3AS/TihRTXMziZVCoEXJiElbG0KIowEFspnrRXq
+kfyUITIiY6wzZ4t+iKzFHoLqElo0ehHbg0JO07FGlOO69zYwBI7EZB2T24NQWUqgle25NUDx2NXq
+3Aqrdy952UKVsTgVRPcwFP2voW==
